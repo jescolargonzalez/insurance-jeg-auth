f57f633bbc8f8a826284b2dfb516e7a7e69f2f8d
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-02-01 23:31:20 +0100

    1rst commit

---
+++ src/main/java/com/insurance/users/service/PolizaService.java
@@ -0,0 +1,16 @@
+package com.insurance.users.service;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolizaService {
+
+
+    public void doSomething(){
+
+//        SecurityContextHolder.getContext().getAuthentication().
+
+    }
+
+}
